567f8504814047c450b601c2ad4b3e24e605cd94
Owen <owen.i.sanchez.r@gmail.com>
2023-01-02 23:09:46 -0600

    starter pack

---
+++ my-app/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main className="App-main">
+        <div className="main-centralDiv">
+          <header className="centralDiv-head"></header>
+          <div className="centralDiv-textField"></div>
+          <div className="centralDiv-chatField">
+            <div className="chatField-inputText">
+              <div className="inputText-Text"></div>
+            </div>
+            <div className="chatField-bottom"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
